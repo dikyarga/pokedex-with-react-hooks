3f3f98ae53a1096d379b348256fe5b550677acfd
dikyarga <dikyarga.id@gmail.com>
2019-11-17 20:03:32 +0700

    create custom hook for fetch single pokemon and implemnt in pokemon page

---
+++ src/hooks/usePokemonFetcher.js
@@ -0,0 +1,83 @@
+import { useEffect, useReducer } from "react";
+import { fetchPokemon } from "../apis";
+const pokemonReducer = (state, event) => {
+  switch (event.type) {
+    case "FETCH":
+      return {
+        ...state,
+        status: "loading"
+      };
+    case "RESOLVE":
+      return {
+        ...state,
+        status: "success",
+        pokemon: event.pokemon
+      };
+    case "REJECT":
+      return {
+        ...state,
+        status: "failure",
+        error: event.error
+      };
+    case "CANCEL":
+      return {
+        ...state,
+        status: "idle"
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  pokemon: {},
+  status: "idle",
+  error: null
+};
+
+const usePokemonFetcher = pokemonName => {
+  const [state, dispatch] = useReducer(pokemonReducer, initialState);
+  const { error, status, pokemon } = state;
+  useEffect(() => {
+    if (state.status === "loading") {
+      let canceled = false;
+
+      fetchPokemon(pokemonName)
+        .then(pokemon => {
+          if (canceled) return;
+          dispatch({
+            type: "RESOLVE",
+            pokemon
+          });
+        })
+        .catch(error => {
+          console.log({
+            error
+          });
+          if (canceled) return;
+          dispatch({
+            type: "REJECT",
+            error
+          });
+        });
+
+      return () => {
+        canceled = true;
+      };
+    }
+  }, [pokemonName, state.status]);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH" });
+  }, []); // on mounted only
+
+  return {
+    error,
+    status,
+    isLoading: status === "loading",
+    pokemon,
+    dispatch
+  };
+};
+
+export default usePokemonFetcher;
--- src/pages/pokemon.js
+++ src/pages/pokemon.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useParams } from "react-router-dom";
+import usePokemonFetcher from "../hooks/usePokemonFetcher";
 function Pokemon() {
   const { pokemonName } = useParams();
-  return <h2>{pokemonName}</h2>;
+  const { isLoading, pokemon } = usePokemonFetcher(pokemonName);
+  return (
+    <Fragment>
+      {isLoading ? <h3>Loading {pokemonName}</h3> : <h1>{pokemon.name}</h1>}
+    </Fragment>
+  );
 }
 
 export default Pokemon;
