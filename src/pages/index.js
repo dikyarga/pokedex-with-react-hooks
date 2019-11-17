d3d2f08d6b29183d040fee036b19f46ee3a6a3fd
dikyarga <dikyarga.id@gmail.com>
2019-11-17 19:56:01 +0700

    implement usePokemonsFetcher on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import usePokemonsFetcher from "../hooks/usePokemonsFetcher";
+
+const Pokemon = ({ pokemon }) => (
+  <Fragment>
+    <Link className="Pokemon-list--item" to={pokemon.name}>
+      {pokemon.name}
+    </Link>
+    <br />
+  </Fragment>
+);
+
 function Homepage() {
-  return <h1>Homepage</h1>;
+  const { error, status, isLoading, pokemons, dispatch } = usePokemonsFetcher();
+
+  return (
+    <div className="App">
+      <h2>{isLoading ? "Fetching" : "Idle"}</h2>
+      <h2>{status === "failure" ? error.message : "Idle"}</h2>
+      <button onClick={() => dispatch({ type: "CANCEL" })}>Cancel</button>
+      <br />
+      {pokemons.map((pokemon, index) => (
+        <Pokemon key={index} index={index} pokemon={pokemon} />
+      ))}
+      {isLoading ? <h3>Fetch more pokemons</h3> : null}
+      <button onClick={() => dispatch({ type: "FETCH" })}>
+        Load more pokemenon
+      </button>
+    </div>
+  );
 }
 
 export default Homepage;
