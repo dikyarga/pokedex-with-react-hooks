ac00856f6c7a7efab1360a2f75039481b42ed375
dikyarga <dikyarga.id@gmail.com>
2019-11-17 19:50:15 +0700

    add react router and setup homepage route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import Homepage from "./pages/index";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+    </Switch>
+  </Router>
+);
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Homepage() {
+  return <h1>Homepage</h1>;
+}
+
+export default Homepage;
