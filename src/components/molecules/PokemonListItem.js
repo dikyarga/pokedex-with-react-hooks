9396a249fd0c92956ceafd141ffd7a218664481c
dikyarga <dikyarga.id@gmail.com>
2019-11-17 20:41:42 +0700

    adjust some styling

--- src/components/molecules/PokemonListItem.js
+++ src/components/molecules/PokemonListItem.js
@@ -4,15 +4,13 @@ import { Link } from "react-router-dom";
 function PokemonListItem({ pokemon, index }) {
   return (
     <Link className="Pokemon-list--item" to={pokemon.name}>
-      <div className="flex items-center">
+      <div className="flex items-center m-2">
         <img
           className="w-10 h-10 rounded-full mr-4"
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${index}.png`}
           alt={pokemon.name}
         />
-        <div className="text-sm">
-          <p className="text-gray-900 leading-none">{pokemon.name}</p>
-        </div>
+        <h3 className="text-gray-900 leading-none text-xl">{pokemon.name}</h3>
       </div>
     </Link>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,27 @@
-import React, { Fragment } from "react";
+import React from "react";
 import usePokemonsFetcher from "../hooks/usePokemonsFetcher";
 import useInfiniteScroll from "../hooks/useInfiniteScroll";
 import PokemonListItem from "../components/molecules/PokemonListItem";
 
 function Homepage() {
-  const { error, status, isLoading, pokemons, dispatch } = usePokemonsFetcher();
+  const { isLoading, pokemons, dispatch } = usePokemonsFetcher();
   useInfiniteScroll(() => dispatch({ type: "FETCH" }));
 
   return (
     <div className="App">
       <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
-        <h1>Super cool page</h1>
+        <h1 className="text-4xl">Pokedex</h1>
+        <h2>{isLoading ? "Fetching" : "Idle"}</h2>
+        <button onClick={() => dispatch({ type: "CANCEL" })}>Cancel</button>
+        <br />
+        {pokemons.map((pokemon, index) => (
+          <PokemonListItem key={index} index={index} pokemon={pokemon} />
+        ))}
+        {isLoading ? <h3>Fetch more pokemons</h3> : null}
+        <button onClick={() => dispatch({ type: "FETCH" })}>
+          Load more pokemenon
+        </button>
       </div>
-      <h2>{isLoading ? "Fetching" : "Idle"}</h2>
-      <h2>{status === "failure" ? error.message : "Idle"}</h2>
-      <button onClick={() => dispatch({ type: "CANCEL" })}>Cancel</button>
-      <br />
-      {pokemons.map((pokemon, index) => (
-        <PokemonListItem key={index} index={index} pokemon={pokemon} />
-      ))}
-      {isLoading ? <h3>Fetch more pokemons</h3> : null}
-      <button onClick={() => dispatch({ type: "FETCH" })}>
-        Load more pokemenon
-      </button>
     </div>
   );
 }
